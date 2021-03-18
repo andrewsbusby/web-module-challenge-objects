bd7a05f7b84d376bb8c101938291471cdebf1648
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-03-18 13:49:59 -0600

    adding answers

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, catagory){
+  return{name, price, catagory};
 }
+console.log(createMenuItem('steak', 10, 'dinner'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -28,7 +29,10 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
+createMenuItem('pizza', 6, 'lunch');
+createMenuItem('hoggie', 5, 'lunch');
+createMenuItem('omlet', 7, 'breakfast');
+console.log(createMenuItem);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
